141d741408b4bfd7472ac3798efcf38481afbb70
Sahil-Khirid <sahilkhirid2211@gmail.com>
2026-07-21 12:18:45 +0530

    feat(eureka): add repository foundation and standalone registry

---
+++ backend/eureka-server/src/test/java/com/skillteam/discovery/EurekaServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.skillteam.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
